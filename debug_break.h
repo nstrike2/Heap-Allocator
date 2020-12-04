0dd57885446a73274bdf5b2823bd62fe22d416c3
nstrike2 <75461391+nstrike2@users.noreply.github.com>
2020-12-03 22:03:40 -0500

    Update debug_break.h

--- debug_break.h
+++ debug_break.h
@@ -3,9 +3,8 @@
  * If running under the debugger, a call to breakpoint() will
  * behave as though execution hit a gdb breakpoint. If not 
  * running under debugger, breakpoint() is a no-op. Call this
- * function in your validate_heap() to break when an error is detected.
+ * function in validate_heap() to break when an error is detected.
  * 
- *  Written by jzelenski, updated Spring 2018
  */
 
 #ifndef DEBUG_BREAK_H
